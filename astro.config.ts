b2aaf2f7fb7d3e15ed9d9d653c96f05f1a082b44
marovole <marovole@hotmail.com>
2026-01-08 08:33:44 +0800

    fix: remove unused import, add page/1 redirects, improve disabled search UX

--- astro.config.ts
+++ astro.config.ts
@@ -15,4 +15,16 @@ export default defineConfig({
     }),
   ],
   trailingSlash: 'always',
+  redirects: {
+    // Redirect /page/1/ to base URL for all source type pages
+    '/annotated-bibliography-example/mla/journal-article/page/1/': '/annotated-bibliography-example/mla/journal-article/',
+    '/annotated-bibliography-example/mla/book/page/1/': '/annotated-bibliography-example/mla/book/',
+    '/annotated-bibliography-example/mla/website/page/1/': '/annotated-bibliography-example/mla/website/',
+    '/annotated-bibliography-example/apa-7/journal-article/page/1/': '/annotated-bibliography-example/apa-7/journal-article/',
+    '/annotated-bibliography-example/apa-7/book/page/1/': '/annotated-bibliography-example/apa-7/book/',
+    '/annotated-bibliography-example/apa-7/website/page/1/': '/annotated-bibliography-example/apa-7/website/',
+    '/annotated-bibliography-example/chicago/journal-article/page/1/': '/annotated-bibliography-example/chicago/journal-article/',
+    '/annotated-bibliography-example/chicago/book/page/1/': '/annotated-bibliography-example/chicago/book/',
+    '/annotated-bibliography-example/chicago/website/page/1/': '/annotated-bibliography-example/chicago/website/',
+  },
 });
